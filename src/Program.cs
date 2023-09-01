ae0490c7a6b53ab2a03732c3c9409b2eccc5c234
Jonas Hansen <jonas.jmh@gmail.com>
2023-09-01 20:48:10 +0200

    Update and add client cert support

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
+using MQTTnet.Client;
 using Prometheus;
 using Serilog;
+using System.Security.Cryptography.X509Certificates;
 using ToMqttNet;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,7 +24,51 @@ services.AddSingleton<DanthermTopicHelper>();
 services.AddMqttConnection(options =>
 {
 	options.NodeId = "dantherm";
-	options.ClientId = "danthermtomqtt";
+
+    var mqttConf = builder.Configuration.GetSection("MqttConnectionOptions");
+    var tcpOptions = new MqttClientTcpOptions
+    {
+        Server = mqttConf["Server"],
+        Port = mqttConf.GetSection("Port")?.Get<int>(),
+    };
+
+    var useTls = mqttConf.GetSection("UseTls")?.Get<bool>() ?? false;
+
+    if (useTls)
+    {
+        var caCrt = new X509Certificate2(mqttConf["CaCrt"]);
+        var clientCrt = X509Certificate2.CreateFromPemFile(mqttConf["ClientCrt"], mqttConf["ClientKey"]);
+
+
+        tcpOptions.TlsOptions = new MqttClientTlsOptions
+        {
+            UseTls = true,
+            SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
+            Certificates = new List<X509Certificate>()
+            {
+                clientCrt, caCrt
+            },
+            CertificateValidationHandler = (certContext) =>
+            {
+                X509Chain chain = new X509Chain();
+                chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+                chain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+                chain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
+                chain.ChainPolicy.VerificationTime = DateTime.Now;
+                chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 0, 0);
+                chain.ChainPolicy.CustomTrustStore.Add(caCrt);
+                chain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
+
+                // convert provided X509Certificate to X509Certificate2
+                var x5092 = new X509Certificate2(certContext.Certificate);
+
+                return chain.Build(x5092);
+            }
+        };
+    }
+
+
+    options.ClientOptions.ChannelOptions = tcpOptions;
 });
 
 
