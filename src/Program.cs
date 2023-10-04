15e8c2efc2026d68f02b93df0f927529e6ea345d
Jonas Hansen <jonas.jmh@gmail.com>
2023-10-04 21:07:13 +0200

    Update ToMqttNet

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using Dantherm2Mqtt;
+using Microsoft.Extensions.Options;
 using MQTTnet.Client;
 using OpenTelemetry.Metrics;
 using System.Security.Cryptography.X509Certificates;
@@ -6,7 +7,6 @@ using ToMqttNet;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 var services = builder.Services;
-
 
 builder.Logging.AddSimpleConsole(options =>
 {
@@ -18,55 +18,55 @@ builder.Logging.AddSimpleConsole(options =>
 services.AddHealthChecks();
 services.AddHostedService<DanthermModBusHandler>();
 services.AddSingleton<DanthermTopicHelper>();
-services.AddMqttConnection(options =>
-{
-	options.NodeId = "dantherm";
+builder.Services.AddMqttConnection()
+	.Configure<IOptions<MqttOptions>>((options, mqttConfI) =>
+	{
+		var mqttConf = mqttConfI.Value;
+		options.NodeId = "dantherm";
 
-    var mqttConf = builder.Configuration.GetSection("MqttConnectionOptions");
-    var tcpOptions = new MqttClientTcpOptions
-    {
-        Server = mqttConf["Server"],
-        Port = mqttConf.GetSection("Port")?.Get<int>(),
-    };
+		builder.Configuration.GetSection("MqttConnectionOptions").Bind(mqttConf);
+		var tcpOptions = new MqttClientTcpOptions
+		{
+			Server = mqttConf.Server,
+			Port = mqttConf.Port,
+		};
 
-    var useTls = mqttConf.GetSection("UseTls")?.Get<bool>() ?? false;
-
-    if (useTls)
-    {
-        var caCrt = new X509Certificate2(mqttConf["CaCrt"]!);
-        var clientCrt = X509Certificate2.CreateFromPemFile(mqttConf["ClientCrt"]!, mqttConf["ClientKey"]);
+		if (mqttConf.UseTls)
+		{
+			var caCrt = new X509Certificate2(mqttConf.CaCrt);
+			var clientCrt = X509Certificate2.CreateFromPemFile(mqttConf.ClientCrt, mqttConf.ClientKey);
 
 
-        tcpOptions.TlsOptions = new MqttClientTlsOptions
-        {
-            UseTls = true,
-            SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
-			ClientCertificatesProvider = new DefaultMqttCertificatesProvider(new List<X509Certificate>()
+			tcpOptions.TlsOptions = new MqttClientTlsOptions
+			{
+				UseTls = true,
+				SslProtocol = System.Security.Authentication.SslProtocols.Tls12,
+				ClientCertificatesProvider = new DefaultMqttCertificatesProvider(new List<X509Certificate>()
 			{
 				clientCrt, caCrt
 			}),
-			CertificateValidationHandler = (certContext) =>
-            {
-                X509Chain chain = new X509Chain();
-                chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
-                chain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
-                chain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
-                chain.ChainPolicy.VerificationTime = DateTime.Now;
-                chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 0, 0);
-                chain.ChainPolicy.CustomTrustStore.Add(caCrt);
-                chain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
+				CertificateValidationHandler = (certContext) =>
+				{
+					X509Chain chain = new X509Chain();
+					chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+					chain.ChainPolicy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
+					chain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
+					chain.ChainPolicy.VerificationTime = DateTime.Now;
+					chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan(0, 0, 0);
+					chain.ChainPolicy.CustomTrustStore.Add(caCrt);
+					chain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
 
-                // convert provided X509Certificate to X509Certificate2
-                var x5092 = new X509Certificate2(certContext.Certificate);
+					// convert provided X509Certificate to X509Certificate2
+					var x5092 = new X509Certificate2(certContext.Certificate);
 
-                return chain.Build(x5092);
-            }
-        };
-    }
+					return chain.Build(x5092);
+				}
+			};
+		}
 
 
-    options.ClientOptions.ChannelOptions = tcpOptions;
-});
+		options.ClientOptions.ChannelOptions = tcpOptions;
+	});
 
 services.AddOpenTelemetry()
 	.WithMetrics(builder =>
@@ -87,3 +87,13 @@ app.MapHealthChecks("/health");
 app.MapPrometheusScrapingEndpoint("/metrics");
 
 app.Run();
+
+public class MqttOptions
+{
+	public int Port { get; set; }
+	public bool UseTls { get; set; }
+	public string Server { get; set; } = null!;
+	public string CaCrt { get; set; } = null!;
+	public string ClientCrt { get; set; } = null!;
+	public string ClientKey { get; set; } = null!;
+}
